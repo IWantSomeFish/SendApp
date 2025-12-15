4a6eb61bcaa68d88b5e7ff55d9e21e0cf32cf0a7
avinogrado <vino74974@gmail.com>
2025-12-15 10:49:58 +0300

    React works (YEY!)

--- src/electron/index.ts
+++ src/electron/index.ts
@@ -2,13 +2,18 @@ import { app, BrowserWindow } from "electron";
 import path from "path";
 
 function createWindow() {
+
   const win = new BrowserWindow({
     width: 1200,
-    height: 800
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
   });
 
-  win.loadFile("index.html");
-  win.webContents.openDevTools();
+  const indexHTML = path.join(__dirname, "..", "index.html");
+  win.loadFile(indexHTML);
 
 }
 
