1e76b5c6b0337b8614b709e93c765460c1dcf0b1
avinogrado <vino74974@gmail.com>
2025-12-17 09:26:49 +0300

    bar changed

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -4,10 +4,10 @@ import IconButtons from './components/naviButton';
 export default function App() {
   return (
 
-    <body style={{backgroundColor: '#3a6181ff'}}>
-    <div style={{ backgroundColor: '#83c5fcff', width: '100%', height: '10vh',marginTop: 0, borderRadius: 20, boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)'}}>
-      <IconButtons style={{ padding: 16 }} iconSrc={'./renderer/components/resources/burger-bar.png'} />
-    </div>
+    <body style={{backgroundColor: '#3a6181ff', margin: 0, padding: 0}}>
+      <div style={{ backgroundColor: '#83c5fcff', width: '100%', height: '10vh',margin:0, boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)'}}>
+        <IconButtons style={{ padding: 16 }} iconSrc={'./renderer/components/resources/burger-bar.png'} />
+      </div>
     </body>
   );
 }
